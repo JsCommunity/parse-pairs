b56b619f8764cdd287e3d5c1506fdf79da1ef729
Julien Fontanet <julien.fontanet@isonoe.net>
2016-11-29 11:43:07 +0100

    chore: split test data from tests

---
+++ src/index.fixtures.js
@@ -0,0 +1,38 @@
+export default {
+  'parse key=value pairs in a string': {
+    string: 'key1=value1 key2=value2',
+    object: {
+      key1: 'value1',
+      key2: 'value2'
+    }
+  },
+  'support simple quotes': {
+    string: "key='value with spaces' 'key with spaces'=value",
+    object: {
+      key: 'value with spaces',
+      'key with spaces': 'value'
+    }
+  },
+  'support double quotes': {
+    string: 'key="value with spaces" "key with spaces"=value',
+    object: {
+      key: 'value with spaces',
+      'key with spaces': 'value'
+    }
+  },
+  'white spaces around pairs are ignored': {
+    string: '  key1=value1  key2=value2  ',
+    object: {
+      key1: 'value1',
+      key2: 'value2'
+    }
+  },
+  '\\n and \\r are valid white spaces': {
+    string: 'key1=value1\nkey2=value2\rkey3=value3',
+    object: {
+      key1: 'value1',
+      key2: 'value2',
+      key3: 'value3'
+    }
+  }
+}
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,39 +1,12 @@
 /* eslint-env jest */
 
+import { forEach } from 'lodash'
+
+import fixtures from './index.fixtures'
 import parsePairs from './'
 
-it('parse key=value pairs in a string', () => {
-  expect(parsePairs('key1=value1 key2=value2')).toEqual({
-    key1: 'value1',
-    key2: 'value2'
-  })
-})
-
-it('support simple quotes', () => {
-  expect(parsePairs("key='value with spaces' 'key with spaces'=value")).toEqual({
-    key: 'value with spaces',
-    'key with spaces': 'value'
-  })
-})
-
-it('support double quotes', () => {
-  expect(parsePairs('key="value with spaces" "key with spaces"=value')).toEqual({
-    key: 'value with spaces',
-    'key with spaces': 'value'
-  })
-})
-
-it('white spaces around pairs are ignored', () => {
-  expect(parsePairs('  key1=value1  key2=value2  ')).toEqual({
-    key1: 'value1',
-    key2: 'value2'
-  })
-})
-
-it('\\n and \\r are valid white spaces', () => {
-  expect(parsePairs('key1=value1\nkey2=value2\rkey3=value3')).toEqual({
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3'
+forEach(fixtures, (data, description) => {
+  it(description, () => {
+    expect(parsePairs(data.string)).toEqual(data.object)
   })
 })
